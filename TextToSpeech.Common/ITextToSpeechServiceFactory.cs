488111f2122fcf8048d97e0559ff8b521ef333b3
Ethan Lindemann <Ethan.Lindemann@Skykick.com>
2019-11-04 18:24:02 -0800

    Updated factory to accept Credentials and specialized Headers

--- TextToSpeech.Common/ITextToSpeechServiceFactory.cs
+++ TextToSpeech.Common/ITextToSpeechServiceFactory.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 
 namespace TextToSpeech.Common
 {
-    public interface ITextToSpeechServiceFactory
+    public interface ITextToSpeechServiceFactory<T>
     {
-        public T Create<T>() where T : ITextToSpeechService, new();
+        public ITextToSpeechServiceFactory<T> AddCredentials(ICredentials credentials);
+        public ITextToSpeechServiceFactory<T> AddHeader(string name, string value);
+
+        /// <summary>
+        /// Creates a TextToSpeechService.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">The service requires authentication.</exception>
+        public ITextToSpeechService Create();
     }
 }
